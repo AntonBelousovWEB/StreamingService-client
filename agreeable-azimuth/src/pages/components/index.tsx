c9d3b686db4c5490fd34706ec279f52c7a652ad9
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2024-02-27 22:59:06 +0200

    user page

--- agreeable-azimuth/src/pages/components/index.tsx
+++ agreeable-azimuth/src/pages/components/index.tsx
@@ -1,41 +1,59 @@
 import React, { useRef, useEffect, useState } from "react";
+import '../styles/App.css';
 import Hls from "hls.js";
+import Header from "./Header";
 
 const Home = () => {
   const playerRef = useRef(null);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    const video = playerRef.current;
-    const hls = new Hls();
-    const url = "http://192.168.1.248:8000/live/hello/index.m3u8";
+    if (playerRef.current) {
+      const video = playerRef.current;
+      const hls = new Hls();
+      const url = "http://192.168.1.248:8000/live/hello/index.m3u8";
 
-    hls.loadSource(url);
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED, function() { video.play(); });
+      hls.loadSource(url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, function() { video.play(); });
 
-    hls.on(Hls.Events.ERROR, (event, data) => {
-      setMessage("Streamer is offline now");
-    });
+      hls.on(Hls.Events.ERROR, (event, data) => {
+        setMessage("Streamer is offline now");
+      });
 
-    return () => {
-      hls.destroy();
-    };
+      return () => {
+        hls.destroy();
+      };
+    }
   }, []);
 
   return (
     <div>
-      {message && <p>{message}</p>}
-      {!message ? (
-        <video
-          width="500"
-          height="300"
-          className="videoCanvas"
-          ref={playerRef}
-          autoPlay={true}
-          controls
-        />
-      ) : null}
+      <Header />
+      <div className="steam_wrap">
+        {message && 
+          <div className="offline_box">
+            <p>{message}</p>
+          </div>
+        }
+        {!message ? (
+          <div id="videoWrapper">
+            <video
+              className="videoCanvas"
+              ref={playerRef}
+              autoPlay={true}
+              controls
+            />
+          </div>
+        ) : null}
+        <div className="streamer_info">
+          <div className="follow_wrap">
+            <h1 className="streamer_name">Adolf Romanovich</h1>
+            <button className="follow_button">Follow</button>
+          </div>
+          <h1 className="streamer_description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium dolorem provident sapiente sint explicabo est earum temporibus repellendus laborum aliquid minima quas debitis, veniam impedit consequatur numquam ipsa suscipit fugiat?</h1>
+        </div>
+      </div>
     </div>
   );
 };

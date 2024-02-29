2f5662f27ac762412c8ef59076ba10524949656c
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2024-02-29 17:37:08 +0200

    forms

--- agreeable-azimuth/src/pages/register/index.tsx
+++ agreeable-azimuth/src/pages/register/index.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Register() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+
     return (
-        <h1>Register Page</h1>
+        <form className="auth_form">
+            {/* {message && <p>{message}</p>} */}
+            <h1 className="title_form">Register</h1>
+            <div className="auth-form_input">
+                <h1>Email:</h1>
+                <input
+                    className="auth_input" 
+                    placeholder="Enter the Email" 
+                    type="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="auth-form_input">
+                <h1>Password:</h1>
+                <input 
+                    className="auth_input" 
+                    placeholder="Enter the Password" 
+                    type="password" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div className="auth-form_input">
+                <h1>Name:</h1>
+                <input 
+                    className="auth_input" 
+                    placeholder="Enter the Name" 
+                    type="text" 
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <button type="submit" className="auth_button">Join</button>
+        </form>
     )
 }

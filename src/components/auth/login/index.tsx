0af0cb4be691768d2c796fd370db2aa8bb301164
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2024-03-04 18:01:59 +0200

    login

--- src/components/auth/login/index.tsx
+++ src/components/auth/login/index.tsx
@@ -1,12 +1,33 @@
 import React, { useState } from 'react';
+import { LOGIN_USER } from '../../../server/mutations/User';
+import { useMutation } from '@apollo/client';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const [logUser] = useMutation(LOGIN_USER);
+    const navigate = useNavigate();
+
+    const Login = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        logUser({
+            variables: {
+                registerUserInput: {
+                    email,
+                    password,
+                }
+            }
+        }).then(({ data }) => {
+            localStorage.setItem("token", data.loginUser.tokenJWT);
+            navigate("/");
+        })
+    }
+
     return (
-        <form className="auth_form">
-            {/* {message && <p>{message}</p>} */}
+        <form className="auth_form" onSubmit={(e) => Login(e)}>
             <h1 className="title_form">Log In</h1>
             <div className="auth-form_input">
                 <h1>Email:</h1>
